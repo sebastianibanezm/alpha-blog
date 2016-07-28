3b881ece7acc299a132d120d7f5b0fcd2fd435a4
Sebastian <sebastianibanezm@gmail.com>
2016-07-28 13:55:29 +0000

    Eliminated redundancies

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -46,6 +48,10 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  
+  def set_article
+    @article = Article.find(params[:id])
   end
   
 end
