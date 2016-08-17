d6e47ef8dd6d14a2e858810922c8e98afdf85a79
Sebastian <sebastianibanezm@gmail.com>
2016-08-17 11:19:22 +0000

    Solved issues with User's table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815032750) do
+ActiveRecord::Schema.define(version: 20160817110406) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,7 +22,11 @@ ActiveRecord::Schema.define(version: 20160815032750) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password_digest"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
