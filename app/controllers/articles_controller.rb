04b3f8201682bab231abe5305fb113e87a57fcd6
Sebastian <sebastianibanezm@gmail.com>
2016-08-24 06:36:57 +0000

    Fixed article creation problem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def create
     @article = Article.new(article_params)
-    @articule.user = current_user
+    @article.user = current_user
       if @article.save
         flash[:success] = "Article was successfully created"
         redirect_to article_path(@article)
