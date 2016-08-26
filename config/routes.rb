cb1a655e0dea571a102435630201cce346030dd0
Sebastian <sebastianibanezm@gmail.com>
2016-08-26 09:01:54 +0000

    Added catgories model and testing

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,9 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 
+resources :categories, except: [:destroy]
+
+
   # Example resource route with options:
   #   resources :products do
   #     member do
